abd008f09abe1b3480ed166722a31c7a9d7de860
Neville Tummon <nt.devs@gmail.com>
2025-12-02 21:06:51 +1100

    refactor projection with transaction

--- internal/inventory/projection.go
+++ internal/inventory/projection.go
@@ -18,21 +18,23 @@ func NewProjection(pool *pgxpool.Pool) *Projection {
 	return &Projection{pool: pool}
 }
 
-func (p *Projection) Apply(ctx context.Context, events ...es.Event) error {
-	return errors.New("not implemented")
+func (p *Projection) SubscribedEvents() []es.EventType {
+	return []es.EventType{
+		api.ItemAddedToCart,
+		api.ItemRemovedFromCart,
+	}
 }
 
 func (p *Projection) ApplyMigration(ctx context.Context) error {
-	// Create inventory_projection schema if it doesn't exist
-	_, err := p.pool.Exec(ctx, `
-		CREATE SCHEMA IF NOT EXISTS inventory_projection;
-	`)
+	tx, err := p.pool.Begin(ctx)
 	if err != nil {
-		return fmt.Errorf("create schema: %w", err)
+		return fmt.Errorf("begin transaction: %w", err)
 	}
 
+	tx.Exec(ctx, `CREATE SCHEMA IF NOT EXISTS inventory_projection;`)
+
 	// Create cart_items table
-	_, err = p.pool.Exec(ctx, `
+	tx.Exec(ctx, `
 		CREATE TABLE IF NOT EXISTS inventory_projection.cart_items (
 			cart_id INTEGER NOT NULL,
 			item_id INTEGER NOT NULL,
@@ -46,12 +48,9 @@ func (p *Projection) ApplyMigration(ctx context.Context) error {
 		ON inventory_projection.cart_items (item_id, checked_out)
 		WHERE checked_out = true;
 	`)
-	if err != nil {
-		return fmt.Errorf("create cart_items table: %w", err)
-	}
 
 	// Create last_processed_position table
-	_, err = p.pool.Exec(ctx, `
+	tx.Exec(ctx, `
 		CREATE TABLE IF NOT EXISTS inventory_projection.last_processed_position (
 			position INTEGER NOT NULL,
 			CONSTRAINT single_row CHECK (position >= 0)
@@ -64,8 +63,9 @@ func (p *Projection) ApplyMigration(ctx context.Context) error {
 			SELECT 1 FROM inventory_projection.last_processed_position
 		);
 	`)
-	if err != nil {
-		return fmt.Errorf("create last_processed_position table: %w", err)
+
+	if err := tx.Commit(ctx); err != nil {
+		return fmt.Errorf("commit transaction: %w", err)
 	}
 
 	return nil
@@ -84,9 +84,6 @@ func (p *Projection) LatestPosition(ctx context.Context) (int64, error) {
 	return position, nil
 }
 
-func (p *Projection) SubscribedEvents() []es.EventType {
-	return []es.EventType{
-		api.ItemAddedToCart,
-		api.ItemRemovedFromCart,
-	}
+func (p *Projection) Apply(ctx context.Context, events ...es.Event) error {
+	return errors.New("not implemented")
 }
